85d4d0483288be6a9391593b316d145c397aa15a
hayamasf <hayama@sfinter.com>
2023-11-07 14:43:57 +0000

    types/types.ts created

---
+++ src/types/types.ts
@@ -0,0 +1,32 @@
+export type NavigationItem = {
+    name: string;
+    href: string;
+    icon: React.ElementType;
+    current: boolean;
+  };
+  
+export  type TeamItem = {
+    id: number;
+    name: string;
+    href: string;
+    initial: string;
+    current: boolean;
+  };
+  
+export type UserNavigationItem = {
+    name: string;
+    href: string;
+  };
+  
+export type NavigationProps = {
+    navigation: NavigationItem[];
+    teams: TeamItem[];
+    sidebarOpen: boolean;
+    setSidebarOpen: (open: boolean) => void;
+  };
+
+export  type TopBarProps = {
+    userNavigation: UserNavigationItem[];
+    setSidebarOpen: (open: boolean) => void;
+  };
+  
