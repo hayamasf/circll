5356e43fd9da01a9c0cd0acf74877985d678c99f
hayamasf <hayama@sfinter.com>
2023-11-07 23:00:00 +0000

    change bg-color to green in contractors.tsx

--- src/app/contractors/page.tsx
+++ src/app/contractors/page.tsx
@@ -9,7 +9,7 @@ export default function Contractors() {
   return (
     <div>
       <main className="py-10">
-        <div className="mx-auto bg-red-200 max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto bg-green-200 max-w-7xl px-4 sm:px-6 lg:px-8">
           {/* Your content */}
           <p>コントラクター</p>
         </div>
