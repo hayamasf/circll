dffccbd1f323db4dd5d620abb99d4859a7afdfd6
hayamasf <hayama@sfinter.com>
2023-11-10 12:46:46 +0000

    🎨 ProfileDropdown out from TopBar.

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,20 +1,13 @@
 import React from "react";
-import { Fragment } from "react";
-
-import { Menu, Transition } from "@headlessui/react";
 
 import Bars3Icon from "@/app/icons/Bars3Icon";
 import BellIcon from "@/app/icons/BellIcon";
-import ChevronDownIcon from "@/app/icons/ChevronDownIcon";
 import MagnifyingGlassIcon from "@/app/icons/MagnifyingGlassIcon";
 
 import { TopBarProps } from "@/types/types";
+import ProfileDropdown from "./ProfileDropdown";
 
-function classNames(...classes: (string | boolean)[]) {
-  return classes.filter(Boolean).join(" ");
-}
-
-const TopBar: React.FC<TopBarProps> = ({ userNavigation, setSidebarOpen }) => {
+const TopBar: React.FC<TopBarProps> = ({ setSidebarOpen }) => {
   return (
     <div className="sticky top-0 z-40 lg:mx-auto lg:max-w-7xl lg:px-8">
       <div className="flex h-16 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-0 lg:shadow-none">
@@ -63,55 +56,7 @@ const TopBar: React.FC<TopBarProps> = ({ userNavigation, setSidebarOpen }) => {
             />
 
             {/* Profile dropdown */}
-            <Menu as="div" className="relative">
-              <Menu.Button className="-m-1.5 flex items-center p-1.5">
-                <span className="sr-only">Open user menu</span>
-                <img
-                  className="h-8 w-8 rounded-full bg-gray-50"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-                <span className="hidden lg:flex lg:items-center">
-                  <span
-                    className="ml-4 text-sm font-semibold leading-6 text-gray-900"
-                    aria-hidden="true"
-                  >
-                    Tom Cook
-                  </span>
-                  <ChevronDownIcon
-                    className="ml-2 h-5 w-5 text-gray-400"
-                    ariaHidden={true}
-                  />
-                </span>
-              </Menu.Button>
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
-              >
-                <Menu.Items className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                  {userNavigation.map((item) => (
-                    <Menu.Item key={item.name}>
-                      {({ active }) => (
-                        <a
-                          href={item.href}
-                          className={classNames(
-                            active ? "bg-gray-50" : "",
-                            "block px-3 py-1 text-sm leading-6 text-gray-900",
-                          )}
-                        >
-                          {item.name}
-                        </a>
-                      )}
-                    </Menu.Item>
-                  ))}
-                </Menu.Items>
-              </Transition>
-            </Menu>
+            <ProfileDropdown />
           </div>
         </div>
       </div>
