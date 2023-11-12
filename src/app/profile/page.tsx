7ecd5752d2fae9421245e40e386988b11983c9eb
hayamasf <hayama@sfinter.com>
2023-11-12 03:32:00 +0000

    ✨ user profile page added.

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { NextPage } from "next";
+
+const Profile: NextPage = () => {
+    return (
+        <div>
+            <main className="py-10">
+                <div className="mx-auto bg-blue-200 max-w-7xl px-4 sm:px-6 lg:px-8">
+                    {/* Your content */}
+                    <p>プロフィール</p>
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default Profile;
