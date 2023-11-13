46575a09e67a8658881e3ff507f1b5d2dd78a711
hayamasf <hayama@sfinter.com>
2023-11-13 23:10:22 +0000

    ✨ added gravatar in ProfileDropdown

--- src/components/ProfileDropdown.tsx
+++ src/components/ProfileDropdown.tsx
@@ -5,6 +5,7 @@ import { Menu, Transition } from "@headlessui/react";
 import ChevronDownIcon from "@/app/icons/ChevronDownIcon";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import Link from "next/link";
+import Image from "next/image";
 
 function classNames(...classes: (string | boolean)[]) {
   return classes.filter(Boolean).join(" ");
@@ -16,12 +17,16 @@ const ProfileDropdown = () => {
   return (
     <Menu as="div" className="relative">
       <Menu.Button className="-m-1.5 flex items-center p-1.5">
-        <span className="sr-only">Open user menu</span>
-        <img
-          className="h-8 w-8 rounded-full bg-gray-50"
-          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-          alt=""
-        />
+        <span className="sr-only">ユーザーメニューを開く</span>
+        <div>
+          {user?.picture && (<Image
+            src={user.picture}
+            alt="プロフィールアバター"
+            className="rounded-full"
+            width={32}
+            height={32}
+          />)}
+        </div>
         <span className="hidden lg:flex lg:items-center">
           <span
             className="ml-4 text-sm font-semibold leading-6 text-gray-900"
