6c66994ebf778262b4736abb3b46c9f2592d6009
hayamasf <hayama@sfinter.com>
2023-11-18 03:13:24 +0000

    ✨ PageHeader introduced.

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,15 @@
+type PageHeaderProps = {
+    title: string;
+}
+
+const PageHeader = ({ title }: PageHeaderProps) => {
+    return (
+        <div className="min-w-0 flex-1">
+            <h1 className="text-xl font-bold leading-7 text-gray-900 sm:truncate sm:text-2xl sm:tracking-tight">
+                {title}
+            </h1>
+        </div>
+    )
+}
+
+export default PageHeader;
