e66209f57b6d98a931ff3457c90dcdd661f286e0
hayamasf <hayama@sfinter.com>
2023-11-19 01:09:56 +0000

    🎨 ContractorRegisterPage updated.

--- src/app/contractors/register/page.tsx
+++ src/app/contractors/register/page.tsx
@@ -1,12 +1,12 @@
 import PageHeader from "@/components/PageHeader";
-
+import ContractorRegistrationForm from "@/components/ContractorRegistrationForm";
 const RegisterPage = () => {
-    return (
-        <>
-            <PageHeader title="業者の新規登録" />
-
-        </>
-    )
-}
+  return (
+    <>
+      <PageHeader title="業者の新規登録" />
+      <ContractorRegistrationForm />
+    </>
+  );
+};
 
 export default RegisterPage;
