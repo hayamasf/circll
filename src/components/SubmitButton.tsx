fac0b45437a844151d107c679a0f419399245eb5
hayamasf <hayama@sfinter.com>
2023-11-25 14:31:31 +0000

    💄 primary button colour changed to slate-800

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -9,7 +9,7 @@ const SubmitButton = ({ label, onClick }: SubmitButtonProps) => {
   return (
     <button
       type="button"
-      className="rounded-md bg-slate-900 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-slate-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      className="rounded-md bg-slate-800 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-slate-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       onClick={onClick}
     >
       {label}
