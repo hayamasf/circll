0a00caa7e229280ec29bdfbbeed1cbe6df933e9a
hayamasf <hayama@sfinter.com>
2023-11-29 06:11:54 +0000

    🎨 Removed onClick from SubmitButton

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -2,15 +2,13 @@ import React from "react";
 
 type SubmitButtonProps = {
   label: string;
-  onClick: () => void;
 };
 
-const SubmitButton = ({ label, onClick }: SubmitButtonProps) => {
+const SubmitButton = ({ label }: SubmitButtonProps) => {
   return (
     <button
-      type="button"
+      type="submit"
       className="rounded-md bg-slate-800 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-slate-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      onClick={onClick}
     >
       {label}
     </button>
