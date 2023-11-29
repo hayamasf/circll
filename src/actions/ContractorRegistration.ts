5f23a199e06d006f74db49d7cf580953778d740a
hayamasf <hayama@sfinter.com>
2023-11-29 06:14:03 +0000

    ✨ action/ContractorRegistration.ts created.

---
+++ src/actions/ContractorRegistration.ts
@@ -0,0 +1,8 @@
+"use server";
+
+import { ContractorRegistrationInputs } from "@/types/types";
+
+export const ContractorRegistration = (formData: ContractorRegistrationInputs) => {
+    console.log(formData);
+
+}
