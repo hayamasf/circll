34c548ac6e79701937de02eb3753d3e837b12d85
hayamasf <hayama@sfinter.com>
2024-01-23 15:31:51 +0000

    ✨ ContractorsList added to Contractors/Page

--- src/app/contractors/page.tsx
+++ src/app/contractors/page.tsx
@@ -1,17 +1,9 @@
-import PageHeader from "@/components/PageHeader";
-import { prisma } from "@/lib/prisma";
-
-async function getContractors() {
-  const allContractors = await prisma.contractor.findMany()
-  return console.log(allContractors);
-}
+import ContractorsList from "@/components/ContractorsList";
 
 export default function Page() {
-  getContractors()
-
   return (
     <>
-      <PageHeader title="業者" />
+      <ContractorsList />
     </>
   );
 }
---
+++ src/components/ContractorsList.tsx
@@ -0,0 +1,62 @@
+import { prisma } from "@/lib/prisma";
+
+export default async function ContractorsList() {
+
+    const contractors = await prisma.contractor.findMany()
+
+    return (
+        <div className="px-4 sm:px-6 lg:px-8">
+            <div className="sm:flex sm:items-center">
+                <div className="sm:flex-auto">
+                    <h1 className="text-base font-semibold leading-6 text-gray-900">業者</h1>
+                    <p className="mt-2 text-sm text-gray-700">
+                        システムに登録されている業者の一覧
+                    </p>
+                </div>
+                <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                    <button
+                        type="button"
+                        className="block rounded-full bg-slate-800 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-slate-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                        新規登録
+                    </button>
+                </div>
+            </div>
+            <div className="-mx-4 mt-8 sm:-mx-0">
+                <table className="min-w-full divide-y divide-gray-300">
+                    <thead>
+                        <tr>
+                            <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                                業者名
+                            </th>
+                            <th
+                                scope="col"
+                                className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
+                            >
+                                代表者
+                            </th>
+                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                住所
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody className="divide-y divide-gray-200 bg-white">
+                        {contractors.map((contractor) => (
+                            <tr key={contractor.id}>
+                                <td className="w-auto py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0">
+                                    {contractor.name}
+                                    <dl className="font-normal lg:hidden">
+                                        <dt className="sr-only sm:hidden">代表者</dt>
+                                        <dd className="mt-1 truncate text-gray-500 sm:hidden">{contractor.title} {contractor.representative}</dd>
+                                    </dl>
+                                </td>
+                                <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">{contractor.title} {contractor.representative}</td>
+                                <td className="px-3 py-4 text-sm text-gray-500">{contractor.prefecture}{contractor.city}{contractor.town}{contractor.address} {contractor.address2}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
