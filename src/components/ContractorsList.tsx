79442e285c03bcf281275a7ccb1a524ab3f8e073
hayamasf <hayama@sfinter.com>
2024-01-24 15:48:35 +0000

    ✅ ContractorsList updated

--- src/components/ContractorsList.tsx
+++ src/components/ContractorsList.tsx
@@ -1,58 +1,114 @@
 import { prisma } from "@/lib/prisma";
 import LinkButton from "./LinkButton";
+import Link from "next/link";
+
+const people = [
+    {
+        name: 'Leslie Alexander',
+        email: 'leslie.alexander@example.com',
+        role: 'Co-Founder / CEO',
+        imageUrl:
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        href: '#',
+        lastSeen: '3h ago',
+        lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+        name: 'Michael Foster',
+        email: 'michael.foster@example.com',
+        role: 'Co-Founder / CTO',
+        imageUrl:
+            'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        href: '#',
+        lastSeen: '3h ago',
+        lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+        name: 'Dries Vincent',
+        email: 'dries.vincent@example.com',
+        role: 'Business Relations',
+        imageUrl:
+            'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        href: '#',
+        lastSeen: null,
+    },
+    {
+        name: 'Lindsay Walton',
+        email: 'lindsay.walton@example.com',
+        role: 'Front-end Developer',
+        imageUrl:
+            'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        href: '#',
+        lastSeen: '3h ago',
+        lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+        name: 'Courtney Henry',
+        email: 'courtney.henry@example.com',
+        role: 'Designer',
+        imageUrl:
+            'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        href: '#',
+        lastSeen: '3h ago',
+        lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+        name: 'Tom Cook',
+        email: 'tom.cook@example.com',
+        role: 'Director of Product',
+        imageUrl:
+            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        href: '#',
+        lastSeen: null,
+    },
+]
+
 
 export default async function ContractorsList() {
 
     const contractors = await prisma.contractor.findMany()
 
     return (
-        <div className="px-4 sm:px-6 lg:px-8">
-            <div className="sm:flex sm:items-center">
-                <div className="sm:flex-auto">
-                    <h1 className="text-base font-semibold leading-6 text-gray-900">業者</h1>
-                    <p className="mt-2 text-sm text-gray-700">
-                        システムに登録されている業者の一覧
-                    </p>
-                </div>
-                <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                    <LinkButton href="/contractors/register">新規登録</LinkButton>
-                </div>
-            </div>
-            <div className="-mx-4 mt-8 sm:-mx-0">
-                <table className="min-w-full divide-y divide-gray-300">
-                    <thead>
-                        <tr>
-                            <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
-                                業者名
-                            </th>
-                            <th
-                                scope="col"
-                                className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
-                            >
-                                代表者
-                            </th>
-                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                住所
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody className="divide-y divide-gray-200 bg-white">
-                        {contractors.map((contractor) => (
-                            <tr key={contractor.id}>
-                                <td className="w-auto py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0">
-                                    {contractor.name}
-                                    <dl className="font-normal lg:hidden">
-                                        <dt className="sr-only sm:hidden">代表者</dt>
-                                        <dd className="mt-1 truncate text-gray-500 sm:hidden">{contractor.title} {contractor.representative}</dd>
-                                    </dl>
-                                </td>
-                                <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">{contractor.title} {contractor.representative}</td>
-                                <td className="px-3 py-4 text-sm text-gray-500">{contractor.prefecture}{contractor.city}{contractor.town}{contractor.address} {contractor.address2}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </div>
+        <ul role="list" className="divide-y divide-gray-100">
+            {people.map((person) => (
+                <li key={person.email} className="relative py-5 hover:bg-gray-50">
+                    <div className="px-4 sm:px-6 lg:px-8">
+                        <div className="mx-auto flex max-w-4xl justify-between gap-x-6">
+                            <div className="flex min-w-0 gap-x-4">
+                                <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={person.imageUrl} alt="" />
+                                <div className="min-w-0 flex-auto">
+                                    <p className="text-sm font-semibold leading-6 text-gray-900">
+                                        <a href={person.href}>
+                                            <span className="absolute inset-x-0 -top-px bottom-0" />
+                                            {person.name}
+                                        </a>
+                                    </p>
+                                    <p className="mt-1 flex text-xs leading-5 text-gray-500">
+                                        {person.email}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="flex shrink-0 items-center gap-x-4">
+                                <div className="hidden sm:flex sm:flex-col sm:items-end">
+                                    <p className="text-sm leading-6 text-gray-900">{person.role}</p>
+                                    {person.lastSeen ? (
+                                        <p className="mt-1 text-xs leading-5 text-gray-500">
+                                            Last seen <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>
+                                        </p>
+                                    ) : (
+                                        <div className="mt-1 flex items-center gap-x-1.5">
+                                            <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                                                <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                                            </div>
+                                            <p className="text-xs leading-5 text-gray-500">Online</p>
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            ))}
+        </ul>
     )
 }
