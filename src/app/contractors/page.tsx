502450786329fcd56824e589a34ad6b2725ce709
hayamasf <hayama@sfinter.com>
2024-01-25 10:14:57 +0000

    ✅ LinkButton & PageHeader Contractors updated

--- src/app/contractors/page.tsx
+++ src/app/contractors/page.tsx
@@ -1,8 +1,14 @@
+import PageHeader from "@/components/PageHeader";
 import ContractorsList from "@/components/ContractorsList";
+import LinkButton from "@/components/LinkButton";
 
 export default function Page() {
   return (
     <>
+      <div className="flex justify-between mb-10 items-center">
+        <PageHeader title="業者" />
+        <LinkButton href="/contractors/register" children={"新規登録"} />
+      </div>
       <ContractorsList />
     </>
   );
--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -4,7 +4,7 @@ type PageHeaderProps = {
 
 export default function PageHeader({ title }: PageHeaderProps) {
   return (
-    <h1 className="mb-8 text-xl font-bold leading-7 text-gray-800 sm:mb-10 sm:truncate sm:text-2xl sm:tracking-tight">
+    <h1 className="text-xl font-bold leading-7 text-gray-800 sm:truncate sm:text-2xl sm:tracking-tight">
       {title}
     </h1>
   );
