4975bfd33b692ad4b28671399cd3096888a31e31
hayamasf <hayama@sfinter.com>
2024-01-30 10:34:01 +0000

    ✨ ContractorEdit page created

---
+++ src/app/contractors/[id]/edit/page.tsx
@@ -0,0 +1,20 @@
+import { prisma } from "@/lib/prisma";
+import PageHeader from "@/components/PageHeader"
+import ContractorForm from "@/components/ContractorForm"
+
+export default async function Page({ params }: { params: { id: string } }) {
+
+    const contractor = await prisma.contractor.findUnique({ where: { id: params.id } });
+
+    if (!contractor) {
+        return <div>該当する業者が見つかりませんでした.</div>
+    }
+
+    return (
+        <div className="container mx-auto max-w-md">
+            <PageHeader title="業者情報の編集" />
+            <p>{contractor.name}</p>
+            <ContractorForm />
+        </div>
+    )
+}
