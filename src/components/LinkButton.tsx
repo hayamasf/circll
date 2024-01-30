1018cacb1f7c2e447dd9b7edae58f58009f7ccb9
hayamasf <hayama@sfinter.com>
2024-01-30 10:34:44 +0000

    💄 LinkButton color changed

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -8,7 +8,7 @@ type LinkButtonProps = {
 
 export default function LinkButton({ href, children }: LinkButtonProps) {
     return (
-        <Link href={href} className="rounded-full bg-slate-800 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-slate-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        <Link href={href} className="rounded-full bg-gray-800 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
             {children}
         </Link>
     )
