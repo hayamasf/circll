30b9ca8f707e5b4633aed48d9bfcefe062f3304f
hayamasf <hayama@sfinter.com>
2024-02-05 14:56:26 +0000

    ✅ Update "/contractors/[id]/edit/page.tsx

--- src/app/contractors/[id]/edit/page.tsx
+++ src/app/contractors/[id]/edit/page.tsx
@@ -2,8 +2,11 @@ import { prisma } from "@/lib/prisma";
 import PageHeader from "@/components/PageHeader"
 import ContractorForm from "@/components/ContractorForm"
 import { updateContractor } from "@/actions/contractor";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default async function Page({ params }: { params: { id: string } }) {
+    const { user } = useUser()
+    const userId = user?.sub || ''
 
     const contractor = await prisma.contractor.findUnique({ where: { id: params.id } });
 
@@ -15,7 +18,7 @@ export default async function Page({ params }: { params: { id: string } }) {
         <div className="container mx-auto max-w-md">
             <PageHeader title="業者情報の編集" />
             <p>{contractor.name}</p>
-            <ContractorForm onSubmit={updateContractor} />
+            <ContractorForm onSubmit={updateContractor} userId={userId} />
         </div>
     )
 }
