c61170f7e10ca4397511b4722723d7ca83551767
hayamasf <hayama@sfinter.com>
2024-02-06 15:26:57 +0000

    🎨 ContractorForm register/edit updated

--- src/app/contractors/[id]/edit/page.tsx
+++ src/app/contractors/[id]/edit/page.tsx
@@ -1,24 +1,14 @@
-import { prisma } from "@/lib/prisma";
 import PageHeader from "@/components/PageHeader"
 import ContractorForm from "@/components/ContractorForm"
-import { updateContractor } from "@/actions/contractor";
-import { useUser } from "@auth0/nextjs-auth0/client";
 
-export default async function Page({ params }: { params: { id: string } }) {
-    const { user } = useUser()
-    const userId = user?.sub || ''
-
-    const contractor = await prisma.contractor.findUnique({ where: { id: params.id } });
-
-    if (!contractor) {
-        return <div>該当する業者が見つかりませんでした.</div>
-    }
+export default function Page() {
 
     return (
         <div className="container mx-auto max-w-md">
             <PageHeader title="業者情報の編集" />
-            <p>{contractor.name}</p>
-            <ContractorForm onSubmit={updateContractor} userId={userId} />
+            {/* <p>{contractor.name}</p> */}
+            <ContractorForm />
         </div>
     )
+
 }
--- src/app/contractors/register/page.tsx
+++ src/app/contractors/register/page.tsx
@@ -1,30 +1,12 @@
-'use client'
-
 import PageHeader from "@/components/PageHeader";
 import ContractorForm from "@/components/ContractorForm";
-import { useUser } from "@auth0/nextjs-auth0/client";
-import { ContractorFormValues } from "@/types/types";
-import { createContractor } from "@/actions/contractor";
 
 export default function Page() {
 
-  const { user } = useUser()
-  const userId = user?.sub || ''
-
-  const handleCreateContractor = (formData: ContractorFormValues) => {
-    createContractor(formData)
-  };
-
-  if (userId) {
-    return (
-      <div className="mx-auto max-w-sm">
-        <PageHeader title="業者の新規登録" />
-        <ContractorForm onSubmit={handleCreateContractor} userId={userId} />
-      </div>
-    );
-
-  }
-
-  return 'loading...'
-
+  return (
+    <div className="mx-auto max-w-sm">
+      <PageHeader title="業者の新規登録" />
+      <ContractorForm />
+    </div>
+  );
 }
