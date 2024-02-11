bd1cd8312438e38d1e389e10f0c7f8f1bb95d7c7
hayamasf <hayama@sfinter.com>
2024-02-11 09:44:11 +0000

    🎨 DesktopSidebar out from Navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -12,11 +12,12 @@ import { classNames } from "@/utils/classNames";
 import Bars3Icon from "@/app/icons/Bars3Icon";
 import BellIcon from "@/app/icons/BellIcon";
 import MagnifyingGlassIcon from "@/app/icons/MagnifyingGlassIcon";
+
+import DesktopSidebar from "./DesktopSidebar";
 import { navigationItems } from "./navigationItems";
 import ProfileDropdown from "./ProfileDropdown";
 
-const Navigation = ({
-}) => {
+const Navigation = () => {
 
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const currentPath = usePathname();
@@ -125,50 +126,7 @@ const Navigation = ({
       </Transition.Root>
 
       {/* Static sidebar for desktop */}
-      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-        {/* Sidebar component, swap this element with another sidebar if you like */}
-        <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
-          <div className="flex h-16 shrink-0 items-center">
-            <img
-              className="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=emerald&shade=200"
-              alt="circll"
-            />
-          </div>
-          <nav className="flex flex-1 flex-col">
-            <ul role="list" className="flex flex-1 flex-col gap-y-7">
-              <li>
-                <ul role="list" className="-mx-2 space-y-1">
-                  {navigationItems.map((item) => (
-                    <li key={item.id}>
-                      <Link
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? "bg-gray-50 text-indigo-600"
-                            : "text-gray-700 hover:text-gray-800 hover:bg-gray-50",
-                          "group flex gap-x-3 rounded-md p-2 text-sm leading-6",
-                        )}
-                      >
-                        <item.icon
-                          className={classNames(
-                            item.current
-                              ? "text-indigo-600"
-                              : "text-gray-400 group-hover:text-gray-800",
-                            "h-6 w-6 shrink-0",
-                          )}
-                          aria-hidden="true"
-                        />
-                        {item.name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
+      <DesktopSidebar navigationItems={navigationItems} />
 
       {/* Topbar */}
       <div className="lg:pl-72">
