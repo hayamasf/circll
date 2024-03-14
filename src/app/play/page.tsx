f5682fa99d79070d6b79baf355e94be66facf234
hayamasf <hayama@sfinter.com>
2024-03-14 13:47:07 +0000

    💄 LegalEntitySelector updated.

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,60 +1,38 @@
 "use client";
 
-const tabs = [
-  { name: 'My Account', href: '#', current: false },
-  { name: 'Company', href: '#', current: false },
-  { name: 'Team Members', href: '#', current: true },
-  { name: 'Billing', href: '#', current: false },
+import React from "react";
 
-  function classNames(...classes: any) {
-    return classes.filter(Boolean).join(' ')
-  }
+import { classNames } from "@/utils/classNames";
+
+const tabs = [
+  { id: 1, name: '会社など法人', href: '#', current: true },
+  { id: 2, name: '個人事業主', href: '#', current: false },
 ]
 
 export default function Page() {
-
   return (
-    <>
-      <div>
-        <div className="sm:hidden">
-          <label htmlFor="tabs" className="sr-only">
-            Select a tab
-          </label>
-          {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
-          <select
-            id="tabs"
-            name="tabs"
-            className="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
-            defaultValue={tabs.find((tab) => tab.current).name}
-          >
+    <div>
+      <div className="block">
+        <div className="border-b border-gray-200">
+          <nav className="-mb-px flex" aria-label="Tabs">
             {tabs.map((tab) => (
-              <option key={tab.name}>{tab.name}</option>
+              <a
+                key={tab.id}
+                href={tab.href}
+                className={classNames(
+                  tab.current
+                    ? 'border-gray-500 text-gray-800'
+                    : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
+                  'w-1/2 border-b-2 py-4 px-1 text-center text-sm font-medium'
+                )}
+                aria-current={tab.current ? 'page' : undefined}
+              >
+                {tab.name}
+              </a>
             ))}
-          </select>
-        </div>
-        <div className="hidden sm:block">
-          <div className="border-b border-gray-200">
-            <nav className="-mb-px flex" aria-label="Tabs">
-              {tabs.map((tab) => (
-                <a
-                  key={tab.name}
-                  href={tab.href}
-                  className={classNames(
-                    tab.current
-                      ? 'border-indigo-500 text-indigo-600'
-                      : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                    'w-1/4 border-b-2 py-4 px-1 text-center text-sm font-medium'
-                  )}
-                  aria-current={tab.current ? 'page' : undefined}
-                >
-                  {tab.name}
-                </a>
-              ))}
-            </nav>
-          </div>
+          </nav>
         </div>
       </div>
-
-    </>
-  );
+    </div>
+  )
 }
--- src/components/LegalEntitySelector.tsx
+++ src/components/LegalEntitySelector.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import { UseFormRegister, FieldErrors } from "react-hook-form";
 import { Client } from "@/types/types";
 
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import { classNames } from "@/utils/classNames";
 
 const entityTypePositions = [
   { id: "before", title: "前" },
@@ -19,165 +19,169 @@ export default function LegalEntitySelector({
   errors: FieldErrors<Client>;
 }) {
 
-  const [tabIndex, setTabIndex] = useState(0);
+  const [types, setTypes] = useState([
+    { id: 1, name: '会社など法人', current: true },
+    { id: 2, name: '個人事業主', current: false },
+  ])
+
+  const toggleCurrent = (tabId: number) => {
+    setTypes(prevTypes =>
+      prevTypes.map((type) => ({ ...type, current: type.id === tabId }))
+    )
+  }
 
   return (
-    <Tabs
-      selectedIndex={tabIndex}
-      onSelect={(index) => { setTabIndex(index) }}
-      className={""}
-      selectedTabClassName="bg-gray-100 font-bold hover:cursor-auto"
-    >
-      <TabList className="flex justify-around text-sm">
-        <Tab
-          className={
-            "w-1/2 text-center rounded-md px-3 py-2 hover:cursor-pointer"
-          }
-        >
-          会社など法人
-        </Tab>
-        <Tab
-          className={
-            "w-1/2 text-center rounded-md px-3 py-2 hover:cursor-pointer"
-          }
-        >
-          個人事業者
-        </Tab>
-      </TabList>
-
-      <TabPanel className={"grid gap-y-8 mt-10"}>
-        {tabIndex}
-        <div className="relative">
-          <label
-            htmlFor="entityType"
-            className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
-          >
-            法人の種類
-          </label>
-          <select
-            id="entityType"
-            {...register("entityType")}
-            className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6`}
-          >
-            <option value="株式会社">株式会社</option>
-            <option value="有限会社">有限会社</option>
-            <option value="合同会社">合同会社</option>
-            <option value="合名会社">合名会社</option>
-            <option value="合資会社">合資会社</option>
-          </select>
-        </div>
-        <div>
-          <label className="ml-3 text-xs text-gray-800">
-            法人格の位置は、法人名称の
-          </label>
-          <fieldset className="mt-0">
-            <legend className="sr-only">法人格の位置</legend>
-            <div className="ml-3 flex items-center space-x-10">
-              {entityTypePositions.map((position) => (
-                <div key={position.id} className="flex items-center">
-                  <input
-                    id={position.id}
-                    type="radio"
-                    value={String(position.id === "before")}
-                    defaultChecked={position.id === "before"}
-                    className="h-4 w-4 border-gray-300 text-gray-600 focus:ring-gray-600"
-                    {...register("isPrefixEntityType")}
-                  />
-                  <label
-                    htmlFor={position.id}
-                    className="ml-3 block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    {position.title}
-                  </label>
-                </div>
+    <>
+      <div>
+        <div className="border-b border-gray-200">
+          <nav className="-mb-px flex" aria-label="Tabs">
+            <ul className="flex w-full">
+              {types.map((type) => (
+                <li key={type.id}
+                  className={classNames(
+                    type.current
+                      ? 'border-gray-500 text-gray-800'
+                      : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 hover: cursor-pointer',
+                    'w-1/2 border-b-2 py-4 px-1 text-center text-sm font-medium'
+                  )}
+                  aria-current={type.current ? 'page' : undefined}
+                  onClick={() => toggleCurrent(type.id)}
+                >
+                  {type.name}
+                </li>
               ))}
-            </div>
-          </fieldset>
+            </ul>
+          </nav>
         </div>
+      </div>
+
+      <div className="relative">
+        <label
+          htmlFor="entityType"
+          className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
+        >
+          法人の種類
+        </label>
+        <select
+          id="entityType"
+          {...register("entityType")}
+          className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6`}
+        >
+          <option value="株式会社">株式会社</option>
+          <option value="有限会社">有限会社</option>
+          <option value="合同会社">合同会社</option>
+          <option value="合名会社">合名会社</option>
+          <option value="合資会社">合資会社</option>
+        </select>
+      </div>
+      <div>
+        <label className="ml-3 text-xs text-gray-800">
+          法人格の位置は、名称の
+        </label>
+        <fieldset className="mt-0">
+          <legend className="sr-only">法人格の位置</legend>
+          <div className="ml-3 flex items-center justify-around">
+            {entityTypePositions.map((position) => (
+              <div key={position.id} className="flex items-center">
+                <input
+                  id={position.id}
+                  type="radio"
+                  value={String(position.id === "before")}
+                  defaultChecked={position.id === "before"}
+                  className="h-4 w-4 border-gray-300 text-gray-600 focus:ring-gray-600"
+                  {...register("isPrefixEntityType")}
+                />
+                <label
+                  htmlFor={position.id}
+                  className="ml-3 block text-sm font-medium leading-6 text-gray-900"
+                >
+                  {position.title}
+                </label>
+              </div>
+            ))}
+          </div>
+        </fieldset>
+      </div>
+      <div className="relative">
+        <label
+          htmlFor="name"
+          className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
+        >
+          名称（法人格は入力しない）
+        </label>
+        <input
+          type="text"
+          id="name"
+          {...register("name", { required: "業者名は必須です" })}
+          className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6`}
+          placeholder="サティスファクトリー"
+        />
+        {errors.name?.message && (
+          <p className="text-xs text-red-500 p-1">{errors.name?.message}</p>
+        )}
+      </div>
+      <div className="grid grid-cols-2 gap-x-3">
         <div className="relative">
           <label
-            htmlFor="name"
+            htmlFor="title"
             className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
           >
-            法人名称（法人格は入力しない）
+            代表者役職名
           </label>
           <input
             type="text"
-            id="name"
-            {...register("name", { required: "業者名は必須です" })}
+            id="title"
+            {...register("title", {
+              required: "代表者役職名は必須です",
+            })}
             className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6`}
-            placeholder="サティスファクトリー"
+            placeholder="代表取締役"
           />
-          {errors.name?.message && (
-            <p className="text-xs text-red-500 p-1">{errors.name?.message}</p>
+          {errors.title?.message && (
+            <p className="text-xs text-red-500 p-1">
+              {errors.title?.message}
+            </p>
           )}
         </div>
-        <div className="grid grid-cols-2 gap-x-3">
-          <div className="relative">
-            <label
-              htmlFor="title"
-              className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
-            >
-              代表者役職名
-            </label>
-            <input
-              type="text"
-              id="title"
-              {...register("title", {
-                required: "代表者役職名は必須です",
-              })}
-              className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6`}
-              placeholder="代表取締役"
-            />
-            {errors.title?.message && (
-              <p className="text-xs text-red-500 p-1">
-                {errors.title?.message}
-              </p>
-            )}
-          </div>
 
-          <div className="relative">
-            <label
-              htmlFor="representative"
-              className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
-            >
-              代表者氏名
-            </label>
-            <input
-              type="text"
-              id="representative"
-              {...register("representative", {
-                required: "代表者氏名は必須です",
-              })}
-              className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6`}
-              placeholder="小松武司"
-            />
-            {errors.representative?.message && (
-              <p className="text-xs text-red-500 p-1">
-                {errors.representative?.message}
-              </p>
-            )}
-          </div>
-        </div>
-      </TabPanel>
-      <TabPanel>
-        {tabIndex}
         <div className="relative">
           <label
-            htmlFor="tradeName"
+            htmlFor="representative"
             className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
           >
-            屋号
+            代表者氏名
           </label>
           <input
             type="text"
-            id="name"
-            {...register("tradeName",)}
+            id="representative"
+            {...register("representative", {
+              required: "代表者氏名は必須です",
+            })}
             className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6`}
-            placeholder="与島クリーン"
+            placeholder="小松武司"
           />
+          {errors.representative?.message && (
+            <p className="text-xs text-red-500 p-1">
+              {errors.representative?.message}
+            </p>
+          )}
         </div>
-      </TabPanel>
-    </Tabs>
+      </div>
+      <div className="relative">
+        <label
+          htmlFor="tradeName"
+          className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
+        >
+          屋号
+        </label>
+        <input
+          type="text"
+          id="name"
+          {...register("tradeName",)}
+          className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6`}
+          placeholder="与島クリーン"
+        />
+      </div>
+    </>
   );
 }
