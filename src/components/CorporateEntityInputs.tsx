040b88b11b2b604b4bdf46db5567190991d0499c
hayamasf <hayama@sfinter.com>
2024-03-16 22:34:41 +0000

    💄 Inputs layout changed in CorporateEntityInputs

--- src/components/CorporateEntityInputs.tsx
+++ src/components/CorporateEntityInputs.tsx
@@ -13,42 +13,44 @@ export default function CorporateEntityInputs({
 }) {
   return (
     <>
-      <div className="relative">
-        <label
-          htmlFor="entityType"
-          className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
-        >
-          法人の種類
-        </label>
-        <select
-          id="entityType"
-          {...register("entityType")}
-          className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6`}
-        >
-          <option value="株式会社">株式会社</option>
-          <option value="有限会社">有限会社</option>
-          <option value="合同会社">合同会社</option>
-          <option value="合名会社">合名会社</option>
-          <option value="合資会社">合資会社</option>
-        </select>
-      </div>
-      <div className="relative">
-        <label
-          htmlFor="isPrefixEntityType"
-          className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
-        >
-          法人格は名称の
-        </label>
-        <select
-          id="entityType"
-          {...register("isPrefixEntityType")}
-          className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6`}
-        >
-          <option value="true">前</option>
-          <option value="false">後ろ</option>
-        </select>
-      </div>
+      <div className="grid grid-cols-2 gap-x-1">
+        <div className="relative">
+          <label
+            htmlFor="entityType"
+            className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
+          >
+            法人の種類
+          </label>
+          <select
+            id="entityType"
+            {...register("entityType")}
+            className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6`}
+          >
+            <option value="株式会社">株式会社</option>
+            <option value="有限会社">有限会社</option>
+            <option value="合同会社">合同会社</option>
+            <option value="合名会社">合名会社</option>
+            <option value="合資会社">合資会社</option>
+          </select>
+        </div>
+        <div className="relative">
+          <label
+            htmlFor="isPrefixEntityType"
+            className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
+          >
+            法人格は名称の
+          </label>
+          <select
+            id="entityType"
+            {...register("isPrefixEntityType")}
+            className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6`}
+          >
+            <option value="true">前</option>
+            <option value="false">後ろ</option>
+          </select>
+        </div>
 
+      </div>
       <div className="relative">
         <label
           htmlFor="name"
@@ -67,7 +69,7 @@ export default function CorporateEntityInputs({
           <p className="text-xs text-red-500 p-1">{errors.name?.message}</p>
         )}
       </div>
-      <div className="grid grid-cols-2 gap-x-3">
+      <div className="grid grid-cols-2 gap-x-1">
         <div className="relative">
           <label
             htmlFor="title"
