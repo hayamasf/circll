9728995e6e0a36adda714cdf42c128f66b65152b
hayamasf <hayama@sfinter.com>
2024-03-17 01:50:50 +0000

    🎨 ContractorForm updated.

--- src/components/ContractorForm.tsx
+++ src/components/ContractorForm.tsx
@@ -1,39 +1,34 @@
 "use client";
 
-import React, { ChangeEvent } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 
 import SubmitButton from "./SubmitButton";
 import CancelButton from "./CancelButton";
 
 import { Contractor } from "@/types/types";
+import { ContractParty } from "@/types/types";
+import LegalEntitySelector from "./LegalEntitySelector";
+import AddressInputs from "./AddressInputs";
 
-import { useUser } from "@auth0/nextjs-auth0/client";
 import { createContractor } from "@/actions/contractor";
-import fetchPrefCityTown from "@/utils/fetchPrefCityTown";
 
 export default function ContractorForm() {
-  const { user } = useUser();
-  const userId = user?.sub || "";
-
-  if (userId) {
-    return <Form userId={userId} />;
-  }
-  return <div className="mt-10">Loading...</div>;
-}
-
-function Form(props: { userId: string }) {
   const {
     register,
+    unregister,
     handleSubmit,
     reset,
     setValue,
     formState: { errors },
-  } = useForm<Contractor>({
+  } = useForm<ContractParty>({
     defaultValues: {
+      entityType: "株式会社",
+      isPrefixEntityType: true,
       name: "",
       title: "",
       representative: "",
+      tradeName: "",
       zipCode: "",
       prefecture: "",
       city: "",
@@ -43,244 +38,30 @@ function Form(props: { userId: string }) {
     },
   });
 
-  const setPrefCityTown = ({
-    pref,
-    city,
-    town,
-  }: {
-    pref: string;
-    city: string;
-    town: string;
-  }) => {
-    setValue("prefecture", pref);
-    setValue("city", city);
-    setValue("town", town);
-  };
-
-  const handleZipCodeInput = async (e: ChangeEvent<HTMLInputElement>) => {
-    const zipCode = e.target.value;
-    if (zipCode.length === 7) {
-      const prefCityTown = await fetchPrefCityTown(zipCode);
-      if (prefCityTown) {
-        setPrefCityTown(prefCityTown);
-      }
-    }
-  };
-
-  const onSubmit = (data: Contractor) => {
+  const onSubmit = (data: any) => {
+    console.log(data);
     createContractor(data);
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="mt-10">
       <div className="grid gap-y-8">
-        <div className="relative">
-          <label
-            htmlFor="name"
-            className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
-          >
-            業者名
-          </label>
-          <input
-            type="text"
-            id="name"
-            {...register("name", { required: "業者名は必須です" })}
-            className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
-            placeholder="株式会社サティスファクトリー"
-          />
-          {errors.name?.message && (
-            <p className="text-xs text-red-500 p-1">{errors.name?.message}</p>
-          )}
-        </div>
-
-        <div className="grid grid-cols-2 gap-x-3">
-          <div className="relative">
-            <label
-              htmlFor="title"
-              className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
-            >
-              代表者役職名
-            </label>
-            <input
-              type="text"
-              id="title"
-              {...register("title", { required: "代表者役職名は必須です" })}
-              className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
-              placeholder="代表取締役"
-            />
-            {errors.title?.message && (
-              <p className="text-xs text-red-500 p-1">
-                {errors.title?.message}
-              </p>
-            )}
-          </div>
-
-          <div className="relative">
-            <label
-              htmlFor="representative"
-              className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
-            >
-              代表者氏名
-            </label>
-            <input
-              type="text"
-              id="representative"
-              {...register("representative", {
-                required: "代表者氏名は必須です",
-              })}
-              className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
-              placeholder="小松武司"
-            />
-            {errors.representative?.message && (
-              <p className="text-xs text-red-500 p-1">
-                {errors.representative?.message}
-              </p>
-            )}
-          </div>
-        </div>
+        <LegalEntitySelector
+          register={register}
+          errors={errors}
+          unregister={unregister}
+        />
         <hr className="my-2" />
-
-        <div className="grid grid-cols-3 gap-x-1">
-          <div className="relative">
-            <label
-              htmlFor="zipCode"
-              className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
-            >
-              郵便番号
-            </label>
-            <input
-              type="text"
-              id="zipCode"
-              {...register("zipCode", { required: "郵便番号は必須です" })}
-              onChange={(e) => {
-                handleZipCodeInput(e);
-              }}
-              className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
-              placeholder="1040032"
-            />
-          </div>
-          <div className="pt-2 text-gray-900 col-span-2 sm:text-sm">
-            ← ハイフンなし、7桁
-          </div>
-          {errors.zipCode?.message && (
-            <p className="text-xs text-red-500 p-1">{errors.zipCode.message}</p>
-          )}
-        </div>
-
-        {/* テスト用ここから */}
-        <div className="grid grid-cols-2 gap-x-1">
-          <div className="relative">
-            <label
-              htmlFor="prefecture"
-              className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
-            >
-              都道府県
-            </label>
-            <input
-              type="text"
-              id="prefecture"
-              {...register("prefecture", { required: "必須" })}
-              className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
-              placeholder="東京都"
-              readOnly
-            />
-            {errors.prefecture?.message && (
-              <p className="text-xs text-red-500 p-1">
-                {errors.prefecture.message}
-              </p>
-            )}
-          </div>
-          <div className="relative">
-            <label
-              htmlFor="city"
-              className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
-            >
-              市区町村
-            </label>
-            <input
-              type="text"
-              id="city"
-              {...register("city", { required: "必須" })}
-              className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
-              placeholder="中央区"
-              readOnly
-            />
-            {errors.city?.message && (
-              <p className="text-xs text-red-500 p-1">{errors.city.message}</p>
-            )}
-          </div>
-        </div>
-
-        {/* ここから */}
-        <div className="grid grid-cols-2 gap-x-1">
-          <div className="relative">
-            <label
-              htmlFor="town"
-              className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
-            >
-              町域
-            </label>
-            <input
-              type="text"
-              id="town"
-              {...register("town", { required: "必須" })}
-              className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
-              placeholder="八丁堀"
-            />
-            {errors.town?.message && (
-              <p className="text-xs text-red-500 p-1">{errors.town.message}</p>
-            )}
-          </div>
-          <div className="relative">
-            <label
-              htmlFor="address"
-              className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
-            >
-              丁目、番地以下
-            </label>
-            <input
-              type="text"
-              id="address"
-              {...register("address", { required: "住所は必須です" })}
-              className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
-              placeholder="三丁目12番8号"
-            />
-            {errors.address?.message && (
-              <p className="text-xs text-red-500 p-1">
-                {errors.address.message}
-              </p>
-            )}
-          </div>
-        </div>
-        {/* ここまで */}
-        <div className="relative">
-          <div className="flex justify-between">
-            <label
-              htmlFor="address2"
-              className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
-            >
-              ビル名など
-            </label>
-            <span
-              className="absolute -top-2 right-2 inline-block bg-white px-1 text-xs text-gray-900"
-              id="address2-optional"
-            >
-              （任意）
-            </span>
-          </div>
-          <input
-            type="text"
-            id="address2"
-            {...register("address2")}
-            className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
-            placeholder="HF八丁堀ビル"
-          />
-        </div>
+        <AddressInputs
+          register={register}
+          errors={errors}
+          setValue={setValue}
+        />
       </div>
       <div className="mt-10 grid gap-y-5">
         <SubmitButton label="登録" />
         <CancelButton label="キャンセル" onClick={() => reset()} />
       </div>
     </form>
-  );
+  )
 }
