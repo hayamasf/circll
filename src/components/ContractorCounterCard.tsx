19f77e5bb25fe9143d282ca0714c73b862d7ccb9
hayamasf <hayama@sfinter.com>
2024-03-21 15:21:45 +0000

    ✨ ContractorCounterCard introduced.

---
+++ src/components/ContractorCounterCard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { prisma } from "@/lib/prisma";
+
+export default async function ContractorCounterCard() {
+  const count = await prisma.contractor.count();
+
+  return (
+    <div className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
+      <dt className="truncate text-sm font-medium text-gray-500">{"テスト"}</dt>
+      <dd className="mt-1 text-center text-3xl font-semibold tracking-tight text-gray-900">
+        {count !== null ? count : "Loading..."}
+      </dd>
+    </div>
+  );
+}
--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,3 +1,5 @@
+import ContractorCounterCard from "./ContractorCounterCard";
+
 const stats = [
   { name: "Total Subscribers", stat: "71,897" },
   { name: "Avg. Open Rate", stat: "58.16%" },
@@ -7,7 +9,7 @@ const stats = [
 export default function Stats() {
   return (
     <div>
-      <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+      <dl className="mt-5 grid grid-cols-2 gap-5 sm:grid-cols-4">
         {stats.map((item) => (
           <div
             key={item.name}
@@ -16,11 +18,12 @@ export default function Stats() {
             <dt className="truncate text-sm font-medium text-gray-500">
               {item.name}
             </dt>
-            <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">
+            <dd className="mt-1 text-center text-3xl font-semibold tracking-tight text-gray-900">
               {item.stat}
             </dd>
           </div>
         ))}
+        <ContractorCounterCard />
       </dl>
     </div>
   );
