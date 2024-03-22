246cf1a9ecd4980b5c9226f45351706bcdd1dca6
hayamasf <hayama@sfinter.com>
2024-03-22 14:19:47 +0000

    🎨 createContractor to create contractor

--- src/actions/contractor.ts
+++ src/actions/contractor.ts
@@ -1,41 +1,63 @@
 "use server";
 
-import { prisma } from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-import { getSession } from "@auth0/nextjs-auth0";
 
+import { prisma } from "@/lib/prisma";
+import { getSession } from "@auth0/nextjs-auth0";
 import { LegalEntity } from "@/types/types";
+import convertToBoolean from "@/utils/convertToBoolean";
 
 export async function createContractor(data: LegalEntity) {
+
+  let isPrefixEntityType
+
+  if (data.isPrefixEntityType !== undefined && data.isPrefixEntityType !== null) {
+    isPrefixEntityType = convertToBoolean(data.isPrefixEntityType)
+  }
+
+  let newContractorId: number | undefined;
+
   try {
     const session = await getSession();
     const userId = session?.user.sub;
 
-    await prisma.contractor.create({
+    if (!userId) {
+      throw new Error("ユーザーIDを確認してください.");
+    }
+
+    const newContractor = await prisma.contractor.create({
       data: {
         createdBy: userId,
-        entityType: data.entityType,
-        isPrefixEntityType: data.isPrefixEntityType,
+        ...(data.entityType && { entityType: data.entityType }),
+        ...(data.isPrefixEntityType && {
+          isPrefixEntityType: isPrefixEntityType,
+        }),
         name: data.name,
-        tradeName: data.tradeName,
-        title: data.title,
-        representative: data.representative,
+        ...(data.title && { title: data.title }),
+        ...(data.representative && { representative: data.representative }),
+        ...(data.tradeName && { tradeName: data.tradeName }),
         zipCode: data.zipCode,
         prefecture: data.prefecture,
         city: data.city,
         town: data.town,
         address: data.address,
-        address2: data.address2,
+        ...(data.address2 && { address2: data.address2 }),
       },
     });
+
+    newContractorId = newContractor.id;
+
     console.log(data);
-    revalidatePath("/contractors");
+    revalidatePath(`/contractors/${newContractorId}`);
   } catch (error) {
     console.error("データの更新に失敗しました.", error);
     throw new Error("データの更新に失敗しました.");
+  } finally {
+    if (newContractorId !== undefined) {
+      redirect(`/contractors/${newContractorId}`);
+    }
   }
-  redirect("/contractors");
 }
 
 export async function updateContractor(id: number, data: Partial<LegalEntity>) {
