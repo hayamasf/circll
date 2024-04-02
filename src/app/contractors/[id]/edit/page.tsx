62721f624265b55a5e90ec8e0df9aa24884fb419
hayamasf <hayama@sfinter.com>
2024-04-02 15:56:26 +0000

    ✅  ContractorEditForm updated

--- src/app/contractors/[id]/edit/page.tsx
+++ src/app/contractors/[id]/edit/page.tsx
@@ -2,22 +2,30 @@ import { prisma } from "@/lib/prisma";
 import PageHeader from "@/components/PageHeader";
 import ContractorEditForm from "@/components/ContractorEditForm";
 
+async function fetchContractor(id: number) {
+  try {
+    const contractor = await prisma.contractor.findUnique({
+      where: { id },
+    });
+    return contractor;
+  } catch (error) {
+    console.error("contractor fetch failed.");
+    throw error;
+  }
+}
+
 export default async function Page({ params }: { params: { id: string } }) {
   const id = Number(params.id);
+  const contractor = await fetchContractor(id);
 
-  if (isNaN(id)) {
-    console.error("idは整数のはずです.");
-    return;
+  if (contractor) {
+    return (
+      <div className="container mx-auto max-w-md">
+        <PageHeader title="業者情報の編集" />
+        {contractor && <ContractorEditForm contractor={contractor} />}
+      </div>
+    );
+  } else {
+    return <div>データ取得中...</div>;
   }
-
-  const contractor = await prisma.contractor.findUnique({
-    where: { id: id },
-  });
-
-  return (
-    <div className="container mx-auto max-w-md">
-      <PageHeader title="業者情報の編集" />
-      {contractor && <ContractorEditForm contractor={contractor} />}
-    </div>
-  );
 }
