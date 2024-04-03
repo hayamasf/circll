b862d3a7be297fcb980653c5a3dfb0e6bdacb92a
hayamasf <hayama@sfinter.com>
2024-04-03 12:54:31 +0000

    🎨 contractor/client/[id] updated

--- src/app/clients/[id]/page.tsx
+++ src/app/clients/[id]/page.tsx
@@ -1,22 +1,10 @@
 import React from "react";
-import { prisma } from "@/lib/prisma";
 import LegalEntityProfile from "@/components/LegalEntityProfile";
-
-async function fetchClient(id: number) {
-  try {
-    const client = await prisma.client.findUnique({
-      where: { id },
-    });
-    return client;
-  } catch (error) {
-    console.error("client fetch failed.", error);
-    throw error;
-  }
-}
+import fetchClientById from "@/utils/fetchClientById";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = Number(params.id);
-  const client = await fetchClient(id);
+  const client = await fetchClientById(id);
 
   if (client) {
     return <LegalEntityProfile entity={client} />;
--- src/app/contractors/[id]/page.tsx
+++ src/app/contractors/[id]/page.tsx
@@ -1,22 +1,10 @@
 import React from "react";
-import { prisma } from "@/lib/prisma";
 import LegalEntityProfile from "@/components/LegalEntityProfile";
-
-async function fetchContractor(id: number) {
-  try {
-    const contractor = await prisma.contractor.findUnique({
-      where: { id },
-    });
-    return contractor;
-  } catch (error) {
-    console.error("contractor fetch failed.");
-    throw error;
-  }
-}
+import fetchContractorById from "@/utils/fetchContractorById";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = Number(params.id);
-  const contractor = await fetchContractor(id);
+  const contractor = await fetchContractorById(id);
 
   if (contractor) {
     return <LegalEntityProfile entity={contractor} />;
