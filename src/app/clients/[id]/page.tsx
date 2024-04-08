ee76aa771aff9fc0c907587bb6952c76a7bf5223
hayamasf <hayama@sfinter.com>
2024-04-08 10:28:04 +0000

    🎨 LinkButton to accept Icon props

--- src/app/clients/[id]/page.tsx
+++ src/app/clients/[id]/page.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import LegalEntityProfile from "@/components/LegalEntityProfile";
 import fetchClientById from "@/utils/fetchClientById";
+import LinkButton from "@/components/LinkButton";
+import { PlusIcon } from "@heroicons/react/24/outline";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = Number(params.id);
   const client = await fetchClientById(id);
 
   if (client) {
-    return <LegalEntityProfile entity={client} />;
-  } else {
-    <div>データを取得中...</div>;
+    return (
+      <div className="mx-auto max-w-3xl">
+        <LegalEntityProfile entity={client} />
+        <LinkButton children={"事業所を登録"} Icon={PlusIcon} href={`./${id}/sites/register`} />
+      </div>
+    );
   }
 }
