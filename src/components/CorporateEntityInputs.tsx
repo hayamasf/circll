1feacfb2945776bdcfe1da2efed65d04ca8c6c79
hayamasf <hayama@sfinter.com>
2024-06-05 15:27:27 +0000

    🐛 select id fixed

--- src/components/CorporateEntityInputs.tsx
+++ src/components/CorporateEntityInputs.tsx
@@ -45,7 +45,7 @@ export default function CorporateEntityInputs() {
             法人格は名称の
           </label>
           <select
-            id="entityType"
+            id="isPrefixEntityType"
             {...register("isPrefixEntityType")}
             className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6`}
           >
