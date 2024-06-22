cc1ebb6bdb9441e0e8c2bede517728dfe15f7831
hayamasf <hayama@sfinter.com>
2024-06-22 10:06:21 +0000

    ✨ PaginationComponent introduced

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,34 +1,54 @@
+"use client";
+
 import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import {
   ArrowLongLeftIcon,
   ArrowLongRightIcon,
 } from "@heroicons/react/20/solid";
 
-export default function Pagination() {
+export default function Pagination({
+  currentPage,
+  limit,
+}: {
+  currentPage: number;
+  limit: number;
+}) {
+  const pathname = usePathname();
+  const previousPage = currentPage > 1 ? currentPage - 1 : 1;
+  const nextPage = currentPage + 1;
+
   return (
     <nav className="flex items-center justify-between my-10 border-t border-gray-200 px-4 sm:px-0">
       <div className="-mt-px flex w-0 flex-1">
-        <a
-          href="#"
-          className="inline-flex items-center border-t-2 border-transparent pr-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-        >
-          <ArrowLongLeftIcon
-            className="mr-3 h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
-          前へ
-        </a>
+        {currentPage > 1 && (
+          <Link
+            href={{
+              pathname: pathname,
+              query: { offset: previousPage, limit: limit },
+            }}
+            className="inline-flex items-center border-t-2 border-transparent pr-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+          >
+            <ArrowLongLeftIcon
+              className="mr-3 h-5 w-5 text-gray-400"
+              aria-hidden="true"
+            />
+            前へ
+          </Link>
+        )}
       </div>
-      <div className="hidden md:-mt-px md:flex">
+
+      {/* <div className="hidden md:-mt-px md:flex">
         <a
           href="#"
           className="inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
         >
           1
-        </a>
-        {/* Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" */}
-        <a
+        </a> */}
+      {/* Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" */}
+      {/* <a
           href="#"
           className="inline-flex items-center border-t-2 border-indigo-500 px-4 pt-4 text-sm font-medium text-indigo-600"
           aria-current="page"
@@ -62,10 +82,13 @@ export default function Pagination() {
         >
           10
         </a>
-      </div>
+      </div> */}
       <div className="-mt-px flex w-0 flex-1 justify-end">
-        <a
-          href="#"
+        <Link
+          href={{
+            pathname: pathname,
+            query: { offset: nextPage, limit: limit },
+          }}
           className="inline-flex items-center border-t-2 border-transparent pl-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
         >
           次へ
@@ -73,7 +96,7 @@ export default function Pagination() {
             className="ml-3 h-5 w-5 text-gray-400"
             aria-hidden="true"
           />
-        </a>
+        </Link>
       </div>
     </nav>
   );
