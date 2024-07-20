80fab9c12fa4d84e875b7a97f7b9cacb3d26bede
hayamasf <hayama@sfinter.com>
2024-07-20 10:26:01 +0000

    🗃️ prisma migrate dev init

---
+++ prisma/migrations/20240720100022_init/migration.sql
@@ -0,0 +1,104 @@
+-- CreateTable
+CREATE TABLE "Contractor" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdBy" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "updatedBy" TEXT,
+    "entityType" TEXT,
+    "isPrefixEntityType" BOOLEAN,
+    "name" TEXT NOT NULL,
+    "title" TEXT,
+    "representative" TEXT,
+    "tradeName" TEXT,
+    "postalCode" TEXT NOT NULL,
+    "prefecture" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "town" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "address2" TEXT,
+
+    CONSTRAINT "Contractor_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Client" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdBy" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "updatedBy" TEXT,
+    "entityType" TEXT,
+    "isPrefixEntityType" BOOLEAN,
+    "name" TEXT NOT NULL,
+    "title" TEXT,
+    "representative" TEXT,
+    "tradeName" TEXT,
+    "postalCode" TEXT NOT NULL,
+    "prefecture" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "town" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "address2" TEXT,
+
+    CONSTRAINT "Client_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Site" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdBy" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "updatedBy" TEXT,
+    "clientId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "postalCode" TEXT NOT NULL,
+    "prefecture" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "town" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "address2" TEXT,
+
+    CONSTRAINT "Site_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Prefecture" (
+    "id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Prefecture_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Municipality" (
+    "id" INTEGER NOT NULL,
+    "prefectureId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Municipality_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "mswLicense" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdBy" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "updatedBy" TEXT,
+    "contractorId" INTEGER NOT NULL,
+    "prefectureId" INTEGER NOT NULL,
+    "municipalityId" INTEGER NOT NULL,
+    "type" INTEGER NOT NULL,
+    "expirationDate" DATE NOT NULL,
+    "licenseUrl" TEXT NOT NULL,
+
+    CONSTRAINT "mswLicense_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Site" ADD CONSTRAINT "Site_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "Client"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Municipality" ADD CONSTRAINT "Municipality_prefectureId_fkey" FOREIGN KEY ("prefectureId") REFERENCES "Prefecture"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
