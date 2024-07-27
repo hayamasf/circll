5dc85ba66c61868147ed41028408867c7b852ef1
hayamasf <hayama@sfinter.com>
2024-07-27 14:16:16 +0000

    ✨ getMswLicenseById introduced

---
+++ src/utils/getMswLicenseById.ts
@@ -0,0 +1,14 @@
+import { prisma } from "@/lib/prisma";
+
+export default async function getMswLicenseById(id: number) {
+  try {
+    const license = await prisma.mswLicense.findUnique(
+      {where: {id}}
+    )
+return license;
+
+  } catch(error) {
+    console.error("一般廃棄物許可情報の取得に失敗しました.");
+    throw error;
+  }
+}
