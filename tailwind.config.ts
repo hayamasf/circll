2df22d9089e5179621fb121ab326087830cee282
hayamasf <hayama@sfinter.com>
2024-09-08 11:19:46 +0000

    💄 tailwind.config.ts updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 const { fontFamily } = require("tailwindcss/defaultTheme");
+import forms from "@tailwindcss/forms";
 
 const config = {
   darkMode: ["class"],
@@ -78,7 +79,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), forms],
 } satisfies Config;
 
 export default config;
