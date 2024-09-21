512c49bd519aecb39904b5886b2e2d9bd48049ff
hayamasf <hayama@sfinter.com>
2024-09-21 04:54:50 +0000

    🗃️ Client/Contracto schema updated

--- src/components/CorporateEntityInputs.tsx
+++ src/components/CorporateEntityInputs.tsx
@@ -108,7 +108,7 @@ export default function CorporateEntityInputs() {
               required: "代表者氏名は必須です",
             })}
             className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6`}
-            placeholder="小松武司"
+            placeholder="小松 武司"
           />
           {errors.representative?.message && (
             <p className="text-xs text-red-500 p-1">
