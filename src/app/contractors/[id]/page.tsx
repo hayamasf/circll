c51f40936052161721beca2dade351470e0e3248
hayamasf <hayama@sfinter.com>
2024-09-23 07:08:59 +0000

    ✨ Breadcrumbs.tsx introduced

--- src/app/contractors/[id]/page.tsx
+++ src/app/contractors/[id]/page.tsx
@@ -1,36 +1,43 @@
 import React from "react";
-import PageHeader from "@/components/PageHeader";
 import LegalEntityProfile from "@/components/LegalEntityProfile";
-import fetchContractorById from "@/utils/fetchContractorById";
+import getContractorById from "@/utils/getContractorById";
+import Breadcrumbs from "@/components/Breadcrumbs";
 import Accordion from "@/components/Accordion";
 import MswLicensesList from "@/components/MswLicensesList";
 import IndustrialWasteLicensesList from "@/components/IndustrialWasteLicensesList";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = Number(params.id);
-  const contractor = await fetchContractorById(id);
+  const contractor = await getContractorById(id);
+
+  const pages = [
+    { name: '業者', href: '/contractors', current: false },
+    // { name: 'Project Nero', href: '#', current: true },
+  ]
 
   const mswLicensesList = contractor
     ? [
-        {
-          title: "一般廃棄物処理業許可",
-          content: <MswLicensesList contractorId={contractor?.id} />,
-        },
-      ]
+      {
+        title: "一般廃棄物処理業許可",
+        content: <MswLicensesList contractorId={contractor?.id} />,
+      },
+    ]
     : [];
 
   const industrialWasteLicensesList = contractor
     ? [
-        {
-          title: "産業廃棄物処理業許可",
-          content: <IndustrialWasteLicensesList contractorId={contractor.id} />,
-        },
-      ]
+      {
+        title: "産業廃棄物処理業許可",
+        content: <IndustrialWasteLicensesList contractorId={contractor.id} />,
+      },
+    ]
     : [];
 
   return (
     <div className="mx-auto max-w-2xl">
-      <PageHeader title="業者" />
+      <div className="py-5">
+        <Breadcrumbs pages={pages} />
+      </div>
       {contractor ? (
         <>
           <LegalEntityProfile entity={contractor} />
