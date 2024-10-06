5eea50db9b6e5d3c9ace37f88fe28d8b7b3042cf
hayamasf <hayama@sfinter.com>
2024-10-06 07:52:56 +0000

    💄 PrefectureMunicipalitySelect updated

--- src/components/PrefectureMunicipalitySelect.tsx
+++ src/components/PrefectureMunicipalitySelect.tsx
@@ -12,7 +12,7 @@ export default function PrefectureMunicipalitySelect({
   disabled?: boolean;
   editForm?: boolean;
 }) {
-  const { register, reset, watch } = useFormContext();
+  const { register, reset, watch, formState: { errors } } = useFormContext();
 
   const [prefectures, setPrefectures] = useState<Prefecture[]>([]);
   const [selectedPrefecture, setSelectedPrefecture] = useState<number | null>(
@@ -52,54 +52,63 @@ export default function PrefectureMunicipalitySelect({
   }, [selectedPrefecture]);
 
   return (
-    <>
-      <div className="relative">
+    <div className="grid grid-cols-2 gap-x-6">
+      <div className="col-span-1">
         <label
           htmlFor="prefectureId"
-          className="absolute z-10 -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
+          className="block text-sm font-medium leading-6 text-gray-900"
         >
           都道府県
         </label>
-        <select
-          id="prefectureId"
-          {...register("prefectureId")}
-          onChange={(e) => setSelectedPrefecture(Number(e.target.value))}
-          disabled={disabled}
-          className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6`}
-        >
-          <option value="" disabled>
-            -- 選択してください --
-          </option>
-          {prefectures.map((prefecture) => (
-            <option key={prefecture.id} value={prefecture.id}>
-              {prefecture.name}
+        <div className="mt-2">
+          <select
+            id="prefectureId"
+            {...register("prefectureId", { required: "選択してください." })}
+            onChange={(e) => setSelectedPrefecture(Number(e.target.value))}
+            disabled={disabled}
+            className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:max-w-xs sm:text-sm sm:leading-6`}
+          >
+            <option value="" disabled>
+              -- 選択 --
             </option>
-          ))}
-        </select>
+            {prefectures.map((prefecture) => (
+              <option key={prefecture.id} value={prefecture.id}>
+                {prefecture.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        {errors.prefectureId && (
+          <p className="text-xs text-red-500 p-1">{errors.prefectureId.message as string}</p>
+        )}
       </div>
-      <div className="relative">
+      <div className="col-span-1">
         <label
           htmlFor="municipalityId"
-          className="absolute z-10 -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
+          className="block text-sm font-medium leading-6 text-gray-900"
         >
           市区町村
         </label>
-        <select
-          id="municipalityId"
-          {...register("municipalityId")}
-          disabled={disabled}
-          className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6`}
-        >
-          <option value="" disabled>
-            -- 選択してください --
-          </option>
-          {municipalities.map((municipality) => (
-            <option key={municipality.id} value={municipality.id}>
-              {municipality.name}
+        <div className="mt-2">
+          <select
+            id="municipalityId"
+            {...register("municipalityId", { required: "選択してください." })}
+            disabled={disabled}
+            className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6`}
+          >
+            <option value="" disabled>
+              -- 選択 --
             </option>
-          ))}
-        </select>
+            {municipalities.map((municipality) => (
+              <option key={municipality.id} value={municipality.id}>
+                {municipality.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        {errors.municipalityId && (<p className="text-xs text-red-500 p-1">{errors.municipalityId.message as string}</p>
+        )}
       </div>
-    </>
+    </div>
   );
 }
