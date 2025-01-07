10948afa47aa2c514581b1e1366698947d718b09
hayamasf <hayama@sfinter.com>
2025-01-07 15:52:12 +0000

    ✅ update autoComplete

--- src/components/ContractRegistrationForm.tsx
+++ src/components/ContractRegistrationForm.tsx
@@ -12,13 +12,13 @@ export default function ContractReistrationForm({ clients }: { clients: Client[]
 
   const methods = useForm<any>({
     defaultValues: {
-      clientName: "",
       clientId: null,
     }
   });
 
   const onSubmit = async (data: any) => console.log(data);
   const [search, setSearch] = useState("");
+  const [selectedClientName, setSelectedClientname] = useState("");
 
   const filteredClients = clients.filter((client) =>
     normalizeString(client.name).includes(normalizeString(search))
@@ -29,7 +29,7 @@ export default function ContractReistrationForm({ clients }: { clients: Client[]
       <form onSubmit={methods.handleSubmit(onSubmit)} className="mt-10">
         <div className="relative">
           <label
-            htmlFor="clientId"
+            htmlFor="clientName"
             className="block text-sm/6 font-medium text-gray-900"
           >
             排出事業者
@@ -38,9 +38,7 @@ export default function ContractReistrationForm({ clients }: { clients: Client[]
             <input
               id="clientName"
               type="text"
-              {...methods.register("clientName", {
-                onChange: (e) => { e.target.value }
-              })}
+              value={search || selectedClientName}
               placeholder="排出事業者名で検索"
               className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-gray-600 sm:text-sm/6"
               onChange={(e) => setSearch(e.target.value)}
@@ -53,8 +51,8 @@ export default function ContractReistrationForm({ clients }: { clients: Client[]
               {filteredClients.map((client) => (
                 <div key={client.id}
                   onClick={() => {
-                    methods.setValue("clientName", formatEntityName(client))
                     methods.setValue("clientId", client.id)
+                    setSelectedClientname(formatEntityName(client))
                     setSearch("")
                   }}
                   className="cursor-pointer px-4 py-2 text-sm hover:bg-gray-100">
