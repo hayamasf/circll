a5c747aafddad186f47a2d1b59ea541822eaf9b0
hayamasf <hayama@sfinter.com>
2025-02-02 13:42:10 +0000

    ✨ PlusButtonDropdownMenu introduced

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -3,6 +3,7 @@ import PageHeader from "@/components/PageHeader";
 import Link from "next/link";
 import { Suspense } from "react";
 import Loading from "../loading";
+import PlusButtonDropdownMenu from "@/components/PlusButtonDropdownMenu";
 import { PlusIcon } from "@heroicons/react/24/outline";
 import ClientsList from "@/components/ClientsList";
 
@@ -18,14 +19,16 @@ export default function Page({
     <div className="mx-auto max-w-2xl">
       <div className="flex justify-between mb-10 items-center">
         <PageHeader title="排出事業者" />
-        <Link
+        <PlusButtonDropdownMenu />
+
+        {/* <Link
           href={"/clients/register"}
           className="flex gap-x-2 items-center text-gray-900 px-2 py-1 rounded-md hover:bg-gray-100"
         >
           <PlusIcon className="h-4 w-4 text-gray-500" />
 
           <span className="text-sm font-semibold">登録する</span>
-        </Link>
+        </Link> */}
       </div>
       <Suspense fallback={<Loading />}>
         <ClientsList offset={offset} limit={limit} />
---
+++ src/components/PlusButtonDropdownMenu.tsx
@@ -0,0 +1,42 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { PlusIcon } from "@heroicons/react/20/solid";
+import Link from 'next/link';
+
+export default function PlusButtonDropdownMenu() {
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <MenuButton className="flex p-1 items-center rounded-full bg-gray-600 text-white hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+          <span className="sr-only">Open options</span>
+
+          <PlusIcon aria-hidden="true" className='size-5' />
+        </MenuButton>
+      </div>
+
+      <MenuItems
+        transition
+        className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+      >
+        <div className="py-1">
+          <MenuItem>
+            <Link
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
+            >
+              会社など法人
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none"
+            >
+              個人事業
+            </Link>
+          </MenuItem>
+
+        </div>
+      </MenuItems>
+    </Menu>
+  )
+}
