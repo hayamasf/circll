92133024b9ca1032f19e3f1904526b767691c9b1
hayamasf <hayama@sfinter.com>
2025-04-17 10:53:18 +0000

    🎨 login page updated

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,16 +1,96 @@
 import { login, signup } from './actions'
 
-export default function LoginPage() {
+export default function Page() {
   return (
-    <main>
-      <form>
-        <label htmlFor="email">Email:</label>
-        <input id="email" name="email" type="email" required />
-        <label htmlFor="password">Password:</label>
-        <input id="password" name="password" type="password" required />
-        <button formAction={login}>Log in</button>
-        <button formAction={signup}>Sign up</button>
-      </form>
-    </main>
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            alt="Your Company"
+            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=slate&shade=600"
+            className="mx-auto h-10 w-auto"
+          />
+          {/* <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+            Sign in to your account
+          </h2> */}
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6">
+            <div>
+              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                メールアドレス
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                  パスワード
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-blue-600 hover:text-blue-500">
+                    パスワードをお忘れの方
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-gray-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className='mt-10'>
+              <button
+                formAction={login}
+                className="flex w-full justify-center cursor-pointer rounded-md bg-gray-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+              >
+                ログイン
+              </button>
+              <button formAction={signup} className='mt-12'>Sign up</button>
+            </div>
+          </form>
+
+          {/* <p className="mt-10 text-center text-sm/6 text-gray-500">
+            Not a member?{' '}
+            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+              Start a 14 day free trial
+            </a>
+          </p> */}
+        </div>
+      </div>
+    </>
   )
 }
+
+
+// export default function LoginPage() {
+//   return (
+//     <main>
+//       <form>
+//         <label htmlFor="email">Email:</label>
+//         <input id="email" name="email" type="email" required />
+//         <label htmlFor="password">Password:</label>
+//         <input id="password" name="password" type="password" required />
+//         <button formAction={login}>Log in</button>
+//         <button formAction={signup}>Sign up</button>
+//       </form>
+//     </main>
+//   )
+// }
