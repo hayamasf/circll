d81df0837183d47a1707fd6df2624890e98c4f63
hayamasf <hayama@sfinter.com>
2025-04-17 10:53:49 +0000

    🎨 AuthLayout updated

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,22 +1,8 @@
-import "../globals.css";
-import { cn } from "@/lib/utils";
-import { Inter as FontSans } from "next/font/google";
-import React from "react";
 
-const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
-
-export default function LoginLayout({ children }: { children: React.ReactNode }) {
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ja">
-      <body className={cn(
-        "min-h-screen bg-background font-sans antialiased",
-        fontSans.variable,
-      )}>
-        {children}
-      </body>
-    </html>
+    <main className="min-h-full">
+      {children}
+    </main>
   )
 }
