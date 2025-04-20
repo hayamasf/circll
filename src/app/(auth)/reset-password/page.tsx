c21d982b29636c9cfd5fec49436e75addd349708
hayamasf <hayama@sfinter.com>
2025-04-20 13:18:46 +0000

    ResetPasswordForm updated

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 import ResetPasswordForm from "@/components/ResetPasswordForm";
 
 export default function Page() {
--- src/components/ResetPasswordForm.tsx
+++ src/components/ResetPasswordForm.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+"use client";
+
+import React, { use } from "react";
+import { useForm } from "react-hook-form";
 
 export default function ResetPasswordForm() {
+  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm();
+
   return (
     <form action="#" method="POST" className="space-y-6">
       <div>
@@ -12,11 +17,27 @@ export default function ResetPasswordForm() {
         </label>
         <div className="mt-2">
           <input
-            id="email"
-            name="email"
-            type="email"
+            id="password"
+            type="password"
+            {...register("password")}
             required
-            autoComplete="email"
+            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 sm:text-sm/6"
+          />
+        </div>
+      </div>
+      <div>
+        <label
+          htmlFor="email"
+          className="block text-sm/6 font-medium text-gray-900"
+        >
+          新しいパスワード（確認用）
+        </label>
+        <div className="mt-2">
+          <input
+            id="confirmPassword"
+            type="password"
+            {...register("confirmPassword")}
+            required
             className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 sm:text-sm/6"
           />
         </div>
@@ -27,7 +48,7 @@ export default function ResetPasswordForm() {
           formAction={""}
           className="flex w-full justify-center cursor-pointer rounded-md bg-gray-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
         >
-          新しいパスワードを設定
+          {isSubmitting ? "更新中..." : "パスワードを更新"}
         </button>
       </div>
     </form>
