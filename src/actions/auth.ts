55cabaa84f431c410f8f429469999ae9f3ca1406
hayamasf <hayama@sfinter.com>
2025-04-20 12:46:53 +0000

    ResetPasswordForm created

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,10 +1,19 @@
 "use server";
+import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
 export async function sendResetPasswordEmail(formData: FormData) {
   const email = formData.get("email") as string;
+  const supabase = await createClient();
 
-  console.log(email);
+  const { error } = await supabase.auth.resetPasswordForEmail(email, {
+    redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/reset-password`,
+  });
 
-  redirect("/forgot-password/confirmation");
+  if (error) {
+    console.error("パスワードリセットメール送信エラー", error.message);
+    throw new Error("メール送信に失敗しました.");
+  }
+
+  redirect("/reset-password");
 }
--- src/app/(auth)/forgot-password/confirmation/page.tsx
+++ src/app/(auth)/forgot-password/confirmation/page.tsx
@@ -3,5 +3,32 @@ import React from "react";
 import Link from "next/link";
 
 export default function Page() {
-  return <div>送信しました</div>;
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          alt="Your Company"
+          src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=slate&shade=600"
+          className="mx-auto h-10 w-auto"
+        />
+        <h1 className="mt-10 text-center text-base font-bold tracking-tight text-gray-900">
+          パスワード再設定メールを送信しました!
+        </h1>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <p className="text-sm">
+          メールが届かない場合、迷惑メールフォルダをご確認下さい.
+        </p>
+        <div className="flex mt-8">
+          <Link
+            href={"/forgot-password"}
+            className="mx-auto text-sm font-semibold text-blue-600 hover:text-blue-500 hover:underline"
+          >
+            パスワードの再設定画面へ
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
