60594b76cdc24c7da144b88695c0c2a60d4b0776
hayamasf <hayama@sfinter.com>
2025-08-03 03:57:07 +0000

    fix handle_new_user  displayName to "displayName",

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
-          alt="Your Company"
+          alt="circll"
           src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=slate&shade=600"
           className="mx-auto h-10 w-auto"
         />
